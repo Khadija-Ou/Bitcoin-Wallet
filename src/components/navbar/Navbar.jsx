890f44539f9fbebd18d558c02756199463eb2e2b
Khadija <khadija@Khadijas-MacBook-Pro.local>
2023-10-31 20:29:46 +0100

    menu changes to match the provided image

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,68 +1,81 @@
-import './navbar.scss'
-import { useState } from 'react'
-import { BiDotsVerticalRounded, BiBookContent } from 'react-icons/bi'
-import { IoIosArrowBack } from 'react-icons/io'
-import { AiOutlineCreditCard } from 'react-icons/ai'
-import { LiaUploadSolid } from 'react-icons/lia'
-import { IoRemoveCircleOutline } from 'react-icons/io5'
+import "./navbar.scss";
+import { useState } from "react";
+import { BiDotsVerticalRounded, BiBookContent } from "react-icons/bi";
+import { IoIosArrowBack } from "react-icons/io";
+import { AiOutlineCreditCard } from "react-icons/ai";
+import { LiaUploadSolid } from "react-icons/lia";
+import { IoRemoveCircleOutline } from "react-icons/io5";
 
 function NavBar() {
-  const [click, setClick] = useState(false)
+  const [click, setClick] = useState(false);
 
-  const handleClick = () => setClick(!click)
+  const handleClick = () => setClick(!click);
 
   return (
     <div>
       {/* Overlay for mobile menu */}
-      {click && <div className='overlay' onClick={handleClick}></div>}
-      <nav className='navbar '>
-        <div className='nav-container page-container'>
-          <div className='nav-arrow'>
+      {click && <div className="overlay" onClick={handleClick}></div>}
+      <nav className="navbar ">
+        <div className="nav-container page-container">
+          <div className="nav-arrow">
             <IoIosArrowBack />
           </div>
 
-          <a href='/' className='nav-logo'>
+          <a href="/" className="nav-logo">
             Bitcoin Wallet
           </a>
 
-          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-            <li className='nav-item'>
-              <a href='/' className='nav-links' onClick={handleClick}>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <a href="/" className="nav-links" onClick={handleClick}>
                 Edit
-                <AiOutlineCreditCard />
+                <AiOutlineCreditCard className="nav-link-icon" />
               </a>
+              <hr className="hr-line" />
             </li>
-            <li className='nav-item'>
-              <a href='/' className='nav-links' onClick={handleClick}>
+            <li className="nav-item">
+              <a href="/" className="nav-links" onClick={handleClick}>
                 Courier info
-                <BiBookContent />
+                <BiBookContent className="nav-link-icon" />
               </a>
+              <hr
+                style={{
+                  background: "#aeb8c4",
+                  height: "1px",
+                  width: "calc(100% - 1rem)",
+                  marginLeft: "auto",
+                }}
+              />
             </li>
-            <li className='nav-item'>
-              <a href='/' className='nav-links' onClick={handleClick}>
+            <li className="nav-item">
+              <a href="/" className="nav-links" onClick={handleClick}>
                 Share
-                <LiaUploadSolid />
+                <LiaUploadSolid className="nav-link-icon" />
               </a>
+              <hr
+                style={{
+                  background: "#aeb8c4",
+                  height: "1px",
+                  width: "calc(100% - 1rem)",
+                  marginLeft: "auto",
+                }}
+              />
             </li>
-            <li className='nav-item'>
-              <a
-                href='/'
-                className='nav-links remove'
-                onClick={handleClick}
-              >
+            <li className="nav-item">
+              <a href="/" className="nav-links remove" onClick={handleClick}>
                 Remove
-                <IoRemoveCircleOutline />
+                <IoRemoveCircleOutline className="nav-link-icon" />
               </a>
             </li>
           </ul>
 
-          <div className='nav-icon' onClick={handleClick}>
+          <div className="nav-icon" onClick={handleClick}>
             <BiDotsVerticalRounded />
           </div>
         </div>
       </nav>
     </div>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
